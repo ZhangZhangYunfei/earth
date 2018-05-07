811949949d14c873ec04a744563cc10d7e1a1c9d
Yunfei Zhang <yunfeizhang@YunfeideMacBook-Pro.local>
2018-05-08 00:18:36 +0800

    a

--- earth-payment/src/test/java/com/yxedu/earth/payment/AppTests.java
+++ earth-payment/src/test/java/com/yxedu/earth/payment/AppTests.java
@@ -6,8 +6,8 @@ import static org.hamcrest.core.Is.is;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
